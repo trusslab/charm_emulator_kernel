cab2ae1fb2ec4392077a79bec935de4a0ad9c550
Yahan Zhou <yahan@google.com>
2017-11-29 16:14:19 -0800

    Remove a warning print in ptrace for adb bugreport
    It prints way too much when doing "adb bugreport" in a userdebug build
    system image, and blocks buildbots from completing CTS.
    
    BUG: 69808686
    
    Change-Id: Ib6a291d7eba773edbecb3c0db09d042b4c9d4d4e
    Signed-off-by: “Yahan Zhou” <yahan@google.com>

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -230,7 +230,10 @@ static int __ptrace_may_access(struct task_struct *task, unsigned int mode)
 	kgid_t caller_gid;
 
 	if (!(mode & PTRACE_MODE_FSCREDS) == !(mode & PTRACE_MODE_REALCREDS)) {
-		WARN(1, "denying ptrace access check without PTRACE_MODE_*CREDS\n");
+		// Do not print the warning here as it will spam when doing "adb bugreport"
+		// with a userdebug Android system image.
+		// BUG: 69808686
+		//WARN(1, "denying ptrace access check without PTRACE_MODE_*CREDS\n");
 		return -EPERM;
 	}
 
