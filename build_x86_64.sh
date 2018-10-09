add4ec1c393b8bf335c62ae8466d78408750ee5f
Seyed Mohammadjavad Seyed Talebi <mjavad@uci.edu>
2018-10-08 19:04:09 -0700

    compiler address fixes

--- build_x86_64.sh
+++ build_x86_64.sh
@@ -6,6 +6,6 @@ xxd -i arch/x86/kernel/charm.dtb > arch/x86/kernel/charm_dtb.h
 #compile the kernel
 export CROSS_COMPILE=x86_64-linux-android-
 export ARCH=x86_64
-export PATH=$PATH:/home/charm/Hamid/x86_64-linux-android-4.9/bin
+export PATH=$PATH:~/Charm/goldfish_AOSP/compiler/x86_64-linux-android-4.9/bin
 make x86_64_ranchu_defconfig
 make -j30
